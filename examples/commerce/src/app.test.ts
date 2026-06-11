71783f1b8e72280c56ccabbcd688d599e3a16e86
Will Chen <7344640+wwwillchen@users.noreply.github.com>
2026-06-11 12:57:39 -0700

    Align commerce deferred stream expectation

--- examples/commerce/src/app.test.ts
+++ examples/commerce/src/app.test.ts
@@ -414,7 +414,6 @@ describe('commerce example', () => {
     expect(response).toMatchObject({
       headers: {
         'Content-Type': 'text/html; charset=utf-8',
-        'Transfer-Encoding': 'chunked',
       },
       status: 200,
     });
