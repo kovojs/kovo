475b315e651ed37fe5196465d46339fdb88e5e54
Will Chen <7344640+wwwillchen@users.noreply.github.com>
2026-07-24 13:25:07 -0700

    chore(security): refresh create-kovo posture identity

--- packages/compiler/src/security/framework-public-runtime-export-posture.generated.ts
+++ packages/compiler/src/security/framework-public-runtime-export-posture.generated.ts
@@ -5648,7 +5648,7 @@ export const frameworkExportPosturePackages: readonly FrameworkExportPosturePack
   ["create-kovo", "0.2.0", [
     ["sha256:65f523e5fc6e4b722b498b204040f16369f1a9dceab9d7854a48bab9b25c700a", [
 
-    ], ["kovo-packed-tree-sha256:acca1dd0a32871bb337025efaca00221a2c394251b657c89fa0585d4ed268d69","kovo-source-tree-sha256:5182c36e133a1e55af6312e80c3c6d5aeac3089563ff9b3e2389661caf11b5e2"]],
+    ], ["kovo-packed-tree-sha256:acca1dd0a32871bb337025efaca00221a2c394251b657c89fa0585d4ed268d69","kovo-source-tree-sha256:eac9d5b507f49cdc3958f9df083fd5b2fb8443682456dab8df3dd75de2a6e437"]],
     ["sha256:7e12bd4ad88296e801d099e9479c8ac74041631b8c5a39e9de873eef8e9f1873", [
 
     ], ["kovo-packed-tree-sha256:acca1dd0a32871bb337025efaca00221a2c394251b657c89fa0585d4ed268d69"]],
