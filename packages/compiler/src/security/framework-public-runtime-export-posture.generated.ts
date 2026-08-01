35d8628f75a53cdda723c08a6e392456338672ae
Will Chen <7344640+wwwillchen@users.noreply.github.com>
2026-07-31 20:04:06 -0700

    chore(security): reseal DB scope proof posture

--- packages/compiler/src/security/framework-public-runtime-export-posture.generated.ts
+++ packages/compiler/src/security/framework-public-runtime-export-posture.generated.ts
@@ -5746,7 +5746,7 @@ export const frameworkExportPosturePackages: readonly FrameworkExportPosturePack
   ["create-kovo", "0.3.0", [
     ["sha256:80d390e85123b76e58a9270f45d1420deaefd869d361bf8181cfa3f310ae6c08", [
 
-    ], ["kovo-packed-tree-sha256:016e1ca8aae30b03f616d0422c436b992f49ca8b1fa659e37f05ef9c6043317c","kovo-source-tree-sha256:a3a99fbf1df3a73c35343b3eaf139620e00bdb7f5f053a511542510875d6fbde"]],
+    ], ["kovo-packed-tree-sha256:016e1ca8aae30b03f616d0422c436b992f49ca8b1fa659e37f05ef9c6043317c","kovo-source-tree-sha256:b8192fed75b87a9772aba695e3582e1182637e361e1f24f5d90e2f2c441dc20e"]],
     ["sha256:ab8a54156a8c7a9662163fa21e51ffe2042988d5457151d70d51c75ac634689e", [
 
     ], ["kovo-packed-tree-sha256:016e1ca8aae30b03f616d0422c436b992f49ca8b1fa659e37f05ef9c6043317c"]],
